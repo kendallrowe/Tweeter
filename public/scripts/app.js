2fe9be3c134fa8313d96acdab4d55d493b1e380a
Kendall Rowe <kendall.rowe312gmail.com>
2019-07-16 21:11:07 +0000

    Refactored code to separate files

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -31,82 +31,43 @@ const data = [
   }
 ]
 
-
 $(document).ready(function(){
 
-  const daysInMonth  = function(month, year) { 
-    return new Date(year, month, 0).getDate(); 
-  } 
-
-  const daysApart = function(date) {
-    const oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
-    const oneHour = 60*60*1000; // minutes*seconds*milliseconds
-    const oneMinute = 60*1000; // seconds*milliseconds
-    const oneSecond = 1000; // milliseconds
-
-    const today = new Date();
-    const daysInThisMonth = daysInMonth(today.getMonth(), today.getFullYear());
-    let unitsApart;
-    let unitsOfTime;
-
-    const diffDays = Math.round(Math.abs((date.getTime() - today.getTime())/(oneDay)));
-    const diffHours = Math.round(Math.abs((date.getTime() - today.getTime())/(oneHour)));
-    const diffMinutes = Math.round(Math.abs((date.getTime() - today.getTime())/(oneMinute)));
-    const diffSeconds = Math.round(Math.abs((date.getTime() - today.getTime())/(oneSecond)));
-
-    if (diffDays / 365 >= 1) {
-      unitsApart = Math.floor(diffDays / 365);
-      unitsOfTime = "years";
-    } else if (diffDays / daysInMonth >= 1) {
-      unitsApart = Math.floor(diffDays / daysInThisMonth)
-      unitsOfTime = "months";
-    } else if (diffDays >= 1) {
-      unitsApart = diffDays;
-      unitsOfTime = "days";
-    } else if (diffHours >= 1) {
-      unitsApart = diffHours;
-      unitsOfTime = "hours";
-    } else if (diffMinutes >= 1){
-      unitsApart = diffMinutes;
-      unitsOfTime = "minutes";
-    } else {
-      unitsApart = diffSeconds;
-      unitsOfTime = "seconds";
-    }
-
-    return `${unitsApart} ${unitsOfTime}`;
-  }
-
+  // Return a string template with the data from a tweet object to be used to append
   function createTweetElement(tweetData) {
-    const dateCreated = new Date(tweetData.created_at);
-    const daysAgo = daysApart(dateCreated);
-    
-    return `
-            <article class="singleTweet">
-              <header class="tweetHeader">
-                <span>
-                  <img src="${tweetData.user.avatars}" alt="Created by Adrien Coquet from the Noun Project">
-                  <h3>${tweetData.user.name}</h4>
-                </span>
-                <h4>${tweetData.user.handle}</h4>
-              </header>
-              
-              <div class="tweetBody">
-                <p>${tweetData.content.text}</p>
-              </div>
-
-              <footer class="tweetFooter">
-                <span>${daysAgo} ago</span>
-                <span class="socials">
-                  <i class="fas fa-flag"></i>
-                  <i class="fas fa-retweet"></i>
-                  <i class="fas fa-heart"></i>
-                </span>
-              </footer>
-            </article>
-          `
+    if (tweetData.length < 0) {
+      // Calculate the days since tweet using helper function
+      const dateCreated = new Date(tweetData.created_at);
+      const daysAgo = daysApart(dateCreated);
+      
+      return `
+              <article class="singleTweet">
+                <header class="tweetHeader">
+                  <span>
+                    <img src="${tweetData.user.avatars}" alt="Created by Adrien Coquet from the Noun Project">
+                    <h3>${tweetData.user.name}</h4>
+                  </span>
+                  <h4>${tweetData.user.handle}</h4>
+                </header>
+                
+                <div class="tweetBody">
+                  <p>${tweetData.content.text}</p>
+                </div>
+  
+                <footer class="tweetFooter">
+                  <span>${daysAgo} ago</span>
+                  <span class="socials">
+                    <i class="fas fa-flag"></i>
+                    <i class="fas fa-retweet"></i>
+                    <i class="fas fa-heart"></i>
+                  </span>
+                </footer>
+              </article>
+            `
+    }
   }
 
+  // Loop through array of tweet objects and append to index page
   const renderTweets = function(tweets) {
     let $tweet;
     // loops through tweets
