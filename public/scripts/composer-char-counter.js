a18137777b3ee64fe58dc5e527fd2d2a16e64cb6
Kendall Rowe <kendall.rowe312@gmail.com>
2019-07-16 16:29:17 +0000

    Added event listener for counter when entering tweet

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  function keyCount(event) {
+    const $tweetInput = $(this);
+    $counter = $tweetInput.siblings("div").children(".counter");
+
+    const currentCount = 140 - $tweetInput.val().length;
+    $counter.text(currentCount.toString());
+
+    if (currentCount < 0) {
+      $counter.css("color", "#EF6969");
+    }
+
+  };
+  
+  $(".new-tweet textarea").on("input", keyCount);
+
+});
+
