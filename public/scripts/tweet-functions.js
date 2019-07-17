a6e626b90715f1fe3cadd5b791f5ac96ed46cc9e
Kendall Rowe <kendall.rowe312gmail.com>
2019-07-17 13:10:33 +0000

    Added refresh tweets upon posting of tweet

--- public/scripts/tweet-functions.js
+++ public/scripts/tweet-functions.js
@@ -1,31 +1,4 @@
 
-// Test / driver code (temporary). Eventually will get this from the server.
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
 // Return a string template with the data from a tweet object to be used to append
 function createTweetElement(tweetData) {
   if (tweetData.length !== 0) {
