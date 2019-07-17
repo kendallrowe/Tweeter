0081bd763431db3be27fcdcc34cb2d694dee5c5c
Kendall Rowe <kendall.rowe312@gmail.com>
2019-07-17 20:56:44 +0000

    Added error message and rename ajaxrequests file

--- public/scripts/new-tweets.js
+++ public/scripts/new-tweets.js
@@ -3,11 +3,14 @@ const $form = $('#newTweetCreate');
 $form.on('submit', function() {
   event.preventDefault();
   const $input = $(this).find("input[type=text], textarea");
+  const $errorMsg = $("#newTweetError");
   // If tweet textarea has been filled
   if (!$input.val()) {
-    alert("Make sure you've written something to tweet!");
+    $errorMsg.children("h4").html("Make sure to add some text to your Tweet and try again.")
+    $input.css("border-bottom", "border-bottom: 3px solid #EF6969")
+    $errorMsg.slideDown(300);
   } else if ($input.val().length > 140){
-    alert("Tweets can only be 140 characters in length.")
+    alert("Oops! Tweets can only be 140 characters are left. Shorten your tweet and try again.");
   } else {
     const serializedInput = $(this).serialize();
     const post_url = $(this).attr("action"); //get form action url
@@ -31,13 +34,3 @@ $form.on('submit', function() {
   }
 });
 
-// AJAX get tweets
-const loadTweets = function() {
-  $.ajax('/tweets', { method: 'GET' })
-  .done(function (tweets) {
-    renderTweets(tweets);
-  })
-  .fail(function (error) {
-    console.log(error);
-  });
-}
